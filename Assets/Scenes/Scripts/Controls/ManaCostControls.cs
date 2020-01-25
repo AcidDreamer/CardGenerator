15c19ef125c679f0379d893142981474e4ce3d2f
NaoumMandrelas <nmandrelas@gmail.com>
2020-01-26 00:09:07 +0200

    -- Mana cost, Fixed fonts

---
+++ Assets/Scenes/Scripts/Controls/ManaCostControls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.Scenes.Scripts
+{
+
+    public class ManaCostControls : MonoBehaviour
+    {
+        public Button OpenBtn;
+        public Button CloseBtn;
+        public GameObject ManaCostPanel;
+
+        void Start()
+        {
+            OpenBtn.onClick.AddListener(() => { ManaCostPanel.SetActive(true); });
+            CloseBtn.onClick.AddListener(() => { ManaCostPanel.SetActive(false); });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
